5e57efcf3f4acc4829b58b41edb1787104e0cb99
Pacifique Mossi <Letsprogrammabit2004@gmail.com>
2026-06-30 14:59:21 +1000

    updated .

---
+++ scripts/update-mpv.sh
@@ -0,0 +1,8 @@
+curl -L -o include/mpv/client.h \
+  "https://raw.githubusercontent.com/mpv-player/mpv/refs/heads/master/include/mpv/client.h"
+curl -L -o include/mpv/render.h \
+  "https://raw.githubusercontent.com/mpv-player/mpv/refs/heads/master/include/mpv/render.h"
+curl -L -o include/mpv/render_gl.h \
+  "https://raw.githubusercontent.com/mpv-player/mpv/refs/heads/master/include/mpv/render_gl.h"
+curl -L -o include/mpv/stream_cb.h \
+  "https://raw.githubusercontent.com/mpv-player/mpv/refs/heads/master/include/mpv/stream_cb.h"
